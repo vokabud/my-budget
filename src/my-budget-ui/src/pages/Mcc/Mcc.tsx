be555c719b92608018f779938b5be2a5a8460a52
vokabud <o.dubakov@gmail.com>
2024-05-11 14:54:34 +0300

    Parse mcc file

--- src/my-budget-ui/src/pages/Mcc/Mcc.tsx
+++ src/my-budget-ui/src/pages/Mcc/Mcc.tsx
@@ -1,12 +1,26 @@
 import { FC } from 'react';
-
 import Section from 'common/Section';
+import data from './mcc.json';
+
+interface IMccGroup {
+  type: string;
+  description: string;
+
+}
+
+interface IMcc {
+  mcc: string;
+  group: IMccGroup;
+  shortDescription: string;
+  fullDescription: string;
+}
 
 const MCC: FC = () => {
-
   return (
     <Section>
-      MCC
+      {data.map((item: IMcc) => (
+        <div key={item.mcc}>{item.mcc} {item.group.type} {item.shortDescription}</div>
+      ))}
     </Section>
   );
 };
