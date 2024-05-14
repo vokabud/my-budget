b32ead3e4d54e847fb67bc9118d02d6d21b84765
vokabud <o.dubakov@gmail.com>
2024-05-14 22:40:07 +0300

    Add ExpenseReport page

---
+++ src/my-budget-ui/src/pages/ExpenseReport/ExpenseReport.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import Section from 'common/Section';
+
+const ExpenseReport: FC = () => {
+  return (
+    <Section>
+      Expense Report
+    </Section>
+  );
+};
+
+export default ExpenseReport;
